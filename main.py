94be72ad0da8c5298fd348e5b25a8cb51ff4c762
MykhailoMykhailiuno <miha290502@gmail.com>
2023-03-06 13:23:17 +0200

    calculation

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-year = 2023
-
-class Book:
-    def __init__(self):
-        self.name = input("Enter book name:  ")
-        self.autor = input("Enter autor name:  ")
-        self.year = int(input("Enter year of a book:  "))
-# Create information print function
-    def books_info(self):
-        """Shows book information"""
-        print(f'{self.name=}'
-        f'Autor = {self.autor}\n'
-        f'Year = {self.year}\n')
-
-# Create calculation
 
 
-# Unite all create menu
+    def calculation(self):
+        print(f'This book {year - self.year} year')
