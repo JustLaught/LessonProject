da5269af3a50cde02c9a4103d4330b93f6951193
JustLaught <lemko2000@gmail.com>
2023-03-06 13:50:07 +0200

    Refresh

---
+++ main.py
@@ -0,0 +1,18 @@
+year = 2023
+
+class Book:
+    def __init__(self):
+        self.name = input("Enter book name:  ")
+        self.autor = input("Enter autor name:  ")
+        self.year = int(input("Enter year of a book:  "))
+# Create information print function
+    def books_info(self):
+        """Shows book information"""
+        print(f'{self.name=}'
+        f'Autor = {self.autor}\n'
+        f'Year = {self.year}\n')
+
+# Create calculation
+
+
+# Unite all create menu
